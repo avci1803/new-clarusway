4c63aaa8dfc2ff7d7bff6b957b741538f6623100
Mehmet Avci <mavci@aligntech.com>
2024-02-13 00:46:33 +0100

    react

--- react/class-notes/rc02_jsx-comp-styling/rc02_jsx/src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      <p>Copyright by Clarusway {new Date().getFullYear()}</p>
-    </footer>
-  );
-};
--- react/class-notes/rc02_props/rc02_props/src/components/Msg.jsx
+++
@@ -1,9 +0,0 @@
-const Msg = ({ isim }) => {
-  return (
-    <div>
-      <h3> Hi my name is {isim}</h3>
-    </div>
-  );
-};
-
-export default Msg;

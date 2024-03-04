1ec2de11da189d5a77903934a70508ce0fbd6711
Mehmet Avci <mavci@aligntech.com>
2024-03-04 16:05:29 +0100

    rc03 module css finished

--- react/class-notes/rc02_props/src/components/Msg.jsx
+++ react/class-notes/rc02_props/src/components/Msg.jsx
@@ -1,5 +1,20 @@
 const Msg = ({ name }) => {
-  return <div>Hi, My name isss {name}</div>;
+  return (
+    <div
+      style={{
+        height: "8px",
+        weight: "10px",
+        margin: "auto",
+        padding: "20px",
+        color: "red",
+        backgroundColor: "gold",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      Hi, My name isss {name}
+    </div>
+  );
 };
 
 export default Msg;
